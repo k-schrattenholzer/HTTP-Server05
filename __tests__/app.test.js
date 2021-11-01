9032cedee8350ddb35360915452a311a216cfce5
‘Katie <ktschr11@gmail.com>
2021-11-01 16:48:58 -0700

    server js set up

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('Mood CRUD API', () => {
+
   it('the Post method creates a new mood, and returns it', async () => {
     const mood = { name:'joyful', intensity:6, overall:'positive' };
 
@@ -9,4 +10,12 @@ describe('Mood CRUD API', () => {
 
     expect(res.body).toEqual({ ...mood, id: expect.any(String) });
   });
+
+//   it('the Get by ID method returns the mood with the given ID', async () => {
+//     const moodWithID = { name:'apathetic', intensity:10, overall:'negative' };
+
+//     const res = await request(app).get('/moods').send(mood);
+
+//     expect(res.body).toEqual({ ...moodWithID, id: expect.any(String) });
+//   });
 });
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-// did this chunk before ready for implementation - will comment back in once a failing test has been written
 
-// const http = require ('http');
-// const app = require('./lib/app');
+const http = require ('http');
+const app = require('./lib/app');
 
-// const PORT = process.env.PORT || 7890;
+const PORT = process.env.PORT || 7890;
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// server.listen(PORT, () => {
-//   console.log(`Server running on port http://localhost:${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
 
